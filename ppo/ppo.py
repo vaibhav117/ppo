2471e13092b197f690322f2a60eca065aa0336a7
vaibhav117 <vaibhavmathur117@gmail.com>
2021-10-22 23:16:26 -0400

    Adding ppo

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -15,10 +15,12 @@ from torch.distributions import MultivariateNormal
 
 from .base_alg import BasePolicyGradient
 
+
 class PPO(BasePolicyGradient):
     """
         This is the PPO class we will use as our model in main.py
     """
+
     def __init__(self, policy_class, env, **hyperparameters):
         """
             Initializes the PPO model, including hyperparameters.
@@ -33,10 +35,9 @@ class PPO(BasePolicyGradient):
         """
         super().__init__(policy_class, env, **hyperparameters)
         # TODO: Initialize critic network
-        self.critic = None
+        self.critic = policy_class(self.obs_dim, 1)
         # Initialize optimizers critic
-        self.critic_optim = None
-
+        self.critic_optim = self.actor_optim
 
     def learn(self, total_timesteps):
         """
@@ -48,10 +49,12 @@ class PPO(BasePolicyGradient):
             Return:
                 None
         """
-        print(f"Learning... Running {self.max_timesteps_per_episode} timesteps per episode, ", end='')
-        print(f"{self.timesteps_per_batch} timesteps per batch for a total of {total_timesteps} timesteps")
-        t_so_far = 0 # Timesteps simulated so far
-        i_so_far = 0 # Iterations ran so far
+        print(
+            f"Learning... Running {self.max_timesteps_per_episode} timesteps per episode, ", end='')
+        print(
+            f"{self.timesteps_per_batch} timesteps per batch for a total of {total_timesteps} timesteps")
+        t_so_far = 0  # Timesteps simulated so far
+        i_so_far = 0  # Iterations ran so far
         while t_so_far < total_timesteps:
             # We're collecting our batch simulations here
             batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens = self.rollout()
@@ -67,10 +70,11 @@ class PPO(BasePolicyGradient):
             self.logger['i_so_far'] = i_so_far
 
             # TODO: Calculate advantage
-            A_k = None
+            V, _ = self.evaluate(batch_obs, batch_acts, batch_rtgs)
+            A_k = batch_rtgs - V
 
             # One of the only tricks we use that isn't in the pseudocode. Normalizing advantages
-            # isn't theoretically necessary, but in practice it decreases the variance of 
+            # isn't theoretically necessary, but in practice it decreases the variance of
             # our advantages and makes convergence much more stable and faster. I added this because
             # solving some environments was too unstable without it.
             A_k = (A_k - A_k.mean()) / (A_k.std() + 1e-10)
@@ -78,8 +82,13 @@ class PPO(BasePolicyGradient):
             # This is the loop where we update our network for some n epochs
             for _ in range(self.n_updates_per_iteration):
                 # TODO: figure out the actor and the critic losses for our algorithm
-                actor_loss = None
-                critic_loss = None
+                V, curr_log_probs = self.evaluate(batch_obs, batch_acts, batch_rtgs)
+                ratios = torch.exp(curr_log_probs - batch_log_probs)
+                surr1 = ratios * A_k
+                surr2 = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * A_k
+
+                actor_loss = (-torch.min(surr1, surr2)).mean()
+                critic_loss = nn.MSELoss()(V, batch_rtgs)
 
                 # Calculate gradients and perform backward propagation for actor network
                 self.actor_optim.zero_grad()
@@ -117,7 +126,7 @@ class PPO(BasePolicyGradient):
                                 batch as a tensor. Shape: (number of timesteps in batch)
         """
         # TODO: Query critic network for a value V for each batch_obs. Shape of V should be same as batch_rtgs
-        V = None
+        V = self.critic(batch_obs).squeeze()
 
         # Calculate the log probabilities of batch actions using most recent actor network.
         # This segment of code is similar to that in get_action()
---
+++ run.py
@@ -0,0 +1,111 @@
+"""
+    This file will run REINFORCE or PPO code
+    with the input seed and environment.
+"""
+
+import gym
+import os
+import argparse
+
+# Import ppo files
+from ppo.ppo import PPO
+from ppo.reinforce import REINFORCE
+from ppo.network import FeedForwardNN
+
+
+def train_ppo(args):
+    """
+        Trains with PPO on specified environment.
+
+        Parameters:
+            args - the arguments defined in main.
+
+        Return:
+            None
+    """
+    # Store hyperparameters and total timesteps to run by environment
+    hyperparameters = {}
+    total_timesteps = 0
+    if args.env == 'Pendulum-v0':
+        hyperparameters = {'timesteps_per_batch': 2048, 'max_timesteps_per_episode': 200, 'gamma': 0.99, 'n_updates_per_iteration': 10,
+                            'lr': 3e-4, 'clip': 0.2, 'save_freq': 1e6, 'seed': args.seed}
+        total_timesteps = 10050000
+    elif args.env == 'BipedalWalker-v3':
+        hyperparameters = {'timesteps_per_batch': 2048, 'max_timesteps_per_episode': 1600, 'gamma': 0.99, 'n_updates_per_iteration': 10,
+                            'lr': 2.5e-4, 'clip': 0.2, 'save_freq': 1e6, 'seed': args.seed}
+        total_timesteps = 1405000
+    elif args.env == 'LunarLanderContinuous-v2':
+        hyperparameters = {'timesteps_per_batch': 1024, 'max_timesteps_per_episode': 1000, 'gamma': 0.999, 'n_updates_per_iteration': 4,
+                            'lr': 2.5e-4, 'clip': 0.2, 'save_freq': 1e6, 'seed': args.seed}
+        total_timesteps = 1005000
+    else:
+        raise ValueError("Unrecognized environment, please specify the hyperparameters first.")
+
+    # Make the environment and model, and train
+    env = gym.make(args.env)
+    model = PPO(FeedForwardNN, env, **hyperparameters)
+    model.learn(total_timesteps)
+
+
+def train_reinforce(args):
+    """
+        Trains with REINFORCE on specified environment.
+
+        Parameters:
+            args - the arguments defined in main.
+
+        Return:
+            None
+    """
+    # Store hyperparameters and total timesteps to run by environment
+    hyperparameters = {}
+    total_timesteps = 0
+    if args.env == 'Pendulum-v0':
+        hyperparameters = {'timesteps_per_batch': 2048, 'max_timesteps_per_episode': 200, 'gamma': 0.99, 'n_updates_per_iteration': 1,
+                            'lr': 3e-4, 'clip': 0.2, 'save_freq': 1e6, 'seed': args.seed}
+        total_timesteps = 10050000
+    elif args.env == 'BipedalWalker-v3':
+        hyperparameters = {'timesteps_per_batch': 2048, 'max_timesteps_per_episode': 1600, 'gamma': 0.99, 'n_updates_per_iteration': 1,
+                            'lr': 2.5e-4, 'clip': 0.2, 'save_freq': 1e6, 'seed': args.seed}
+        total_timesteps = 1405000
+    elif args.env == 'LunarLanderContinuous-v2':
+        hyperparameters = {'timesteps_per_batch': 1024, 'max_timesteps_per_episode': 1000, 'gamma': 0.999, 'n_updates_per_iteration': 1,
+                            'lr': 2.5e-4, 'clip': 0.2, 'save_freq': 1e6, 'seed': args.seed}
+        total_timesteps = 1005000
+    else:
+        raise ValueError("Unrecognized environment, please specify the hyperparameters first.")
+
+    # Make the environment and model, and train
+    env = gym.make(args.env)
+    model = REINFORCE(FeedForwardNN, env, **hyperparameters)
+    model.learn(total_timesteps)
+
+
+def main(args):
+    """
+        An intermediate function that will call either REINFORCE learn or PPO learn.
+
+        Parameters:
+            args - the arguments defined below
+
+        Return:
+            None
+    """
+    if args.alg == 'PPO':
+        train_ppo(args)
+    elif args.alg == 'reinforce':
+        train_reinforce(args)
+    else:
+        raise ValueError(f'Algorithm {args.alg} not defined; options are reinforce or PPO.')
+
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--alg', dest='alg', type=str, default='reinforce')        # Formal name of our algorithm
+    parser.add_argument('--seed', dest='seed', type=int, default=None)             # An int for our seed
+    parser.add_argument('--env', dest='env', type=str, default='')                 # Formal name of environment
+
+    args = parser.parse_args()
+
+    main(args)
